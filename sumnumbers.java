4ee9bb891a3ceb252061ca86c45b547d20ca05af
vani105 <srivaniathota@gmail.com>
2024-11-22 14:55:12 +0530

    Add files via upload

---
+++ sumnumbers.java
@@ -0,0 +1,10 @@
+public class SumNumbers {
+    public static void main(String[] args) {
+        int num = 10;
+        int sum = 0; 
+        for (int i = 1; i <= num; i++) {
+            sum += i; 
+        }
+        System.out.println("Sum is: " + sum); 
+    }
+}
