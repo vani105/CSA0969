9f7bfb95febd4696984dc5e8437e3af0c34a464b
vani105 <srivaniathota@gmail.com>
2024-11-22 14:21:54 +0530

    Add files via upload

---
+++ maxscore.java
@@ -0,0 +1,8 @@
+public class maxscore{
+    public static void main(String[] args) {
+        int score = 423;
+        int total_score = 500;
+        float maxper = ((float)score/total_score)*100;
+        System.out.println("maximum score is" +maxper +"%");
+    }
+}
